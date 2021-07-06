6b41082c327fbad961c6b7ff85aabc0d21aca708
riccardompesce <riccardompesce@gmail.com>
2021-07-06 13:00:35 +0200

    Completed chap 1 ex 2 and 3

---
+++ chap1/ex-2.py
@@ -0,0 +1,39 @@
+def gcd(m, n):
+    while m % n != 0:
+        m, n = n, m % n
+    return n
+
+class Fraction:
+    def __init__(self, top, bottom):
+        # Reducing fraction straight away
+        cmmn = gcd(top, bottom)
+        
+        self.num = top
+        self.den = bottom
+
+    def __str__(self):
+        return "{:d}/{:d}".format(self.num, self.den)
+
+    def __eq__(self, other_fraction):
+        first_num = self.num * other_fraction.den
+        second_num = other_fraction.num * self.den
+
+        return first_num == second_num
+
+    def __add__(self, other_fraction):
+        new_num = self.num * other_fraction.den + self.den * other_fraction.num
+        new_den = self.den * other_fraction.den
+        
+        # No need to reduce anymore 
+        return Fraction(new_num, new_den)
+
+    def show(self):
+        print("{:d}/{:d}".format(self.num, self.den))
+
+    # Here are the methods requested
+
+    def get_num(self):
+        return self.num
+    
+    def get_den(self):
+        return self.den
