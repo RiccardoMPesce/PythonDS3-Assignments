9541b9fa4742d1dfccae0ba4ad7b9f6477f0626d
riccardompesce <riccardompesce@gmail.com>
2021-09-17 22:09:06 +0200

    Starting the Hilbert curve

--- chapter5/hilbert.py
+++ chapter5/hilbert.py
@@ -5,49 +5,9 @@ HEIGHT = 1024
 ORDER = 3
 LEN = WIDTH // 2
 
-def hilbert(order, t, start=None, l=WIDTH//2, rot=None):
-    if order == 1:
-        draw_curve(t, start, l, rot) 
-    else:
-        # t.left(90)
-        hilbert(order - 1, t, l=l//4, rot="r")
-        t.right(90)
-        t.forward(l // 2)
-        hilbert(order - 1, t, l=l//4)
-        t.left(90)
-        t.forward(l // 2)
-        hilbert(order - 1, t, l=l//4)
-        # t.left(90)
-        t.forward(l // 2)
-        hilbert(order - 1, t, l=l//4, rot="l")
-
-def draw_curve(t, start=None, l=WIDTH//2, rot=None):
-    if start is not None:
-        t.up()
-        t.goto(start)
-        t.down()
+def hilbert(order, t, angle=90, step=WIDTH // 2):
+    pass
     
-    if rot is None:
-        t.seth(90)
-        t.forward(l)
-        t.right(90)
-        t.forward(l)
-        t.right(90)
-        t.forward(l)
-    elif rot.lower() in ["l", "left"]:
-        t.seth(180)
-        t.forward(l)
-        t.left(90)
-        t.forward(l)
-        t.left(90)
-        t.forward(l)
-    elif rot.lower() in ["r", "right"]:
-        t.seth(0)
-        t.forward(l)
-        t.left(90)
-        t.forward(l)
-        t.left(90)
-        t.forward(l)
 
 def main():
     t = turtle.Turtle()
@@ -60,7 +20,7 @@ def main():
     t.goto((WIDTH // 4, HEIGHT // 4))
     t.down()
 
-    hilbert(2, t)
+    hilbert(3, t)
 
     s.exitonclick()
 
