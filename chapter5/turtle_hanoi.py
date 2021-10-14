98f610270462b7648186a859fae63e542dd97aa0
riccardompesce <riccardompesce@gmail.com>
2021-10-14 22:36:00 +0200

    TODO TODO todo.....

--- chapter5/turtle_hanoi.py
+++ chapter5/turtle_hanoi.py
@@ -1,20 +1,24 @@
 import turtle 
 
+from pythonds3 import Stack
+
 SCREEN_WIDTH = 2048
 SCREEN_HEIGHT = 2048
 HEIGHT = 3
 TURTLE_HEIGHT = 10
 
-def move_tower(height, from_pole, to_pole, with_pole, turtles):
-    assert len(turtles) == height
-
+def move_tower(height, from_pole, to_pole, with_pole):
+    if from_pole.is_empty() and to_pole.is_empty() and with_pole.is_empty():
+        for i in range(1, height + 1):
+            from_pole.push(f"disk{i}")
     if height >= 1:
         move_tower(height - 1, from_pole, with_pole, to_pole)
         move_disk(from_pole, to_pole)
+        to_pole.push(from_pole.pop())
         move_tower(height - 1, with_pole, to_pole, from_pole)
 
 def move_disk(from_p, to_p):
-    print("moving disk from", from_p, "to", to_p)
+    print("moving disk from", from_p.name, "to", to_p.name)
 
 def main():
     turtles = [turtle.Turtle() for _ in range(HEIGHT)] 
