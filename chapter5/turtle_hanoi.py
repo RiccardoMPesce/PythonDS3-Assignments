e05ffa777bbee43fbd102b6d8c816a8deae83041
riccardompesce <riccardompesce@gmail.com>
2021-10-15 16:05:51 +0200

    Written next question

--- chapter5/turtle_hanoi.py
+++ chapter5/turtle_hanoi.py
@@ -2,11 +2,6 @@ import turtle
 
 from pythonds3 import Stack
 
-SCREEN_WIDTH = 2048
-SCREEN_HEIGHT = 2048
-HEIGHT = 3
-TURTLE_HEIGHT = 10
-
 def move_tower(height, from_pole, to_pole, with_pole):
     if height >= 1:
         move_tower(height - 1, from_pole, with_pole, to_pole)
@@ -20,7 +15,11 @@ def main():
     t1 = turtle.Turtle()
     t2 = turtle.Turtle()
     t3 = turtle.Turtle()
-    
+
+    p1 = turtle.Turtle()
+    p2 = turtle.Turtle()
+    p = turtle.Turtle()
+
     turtle.exitonclick()
 
 if __name__ == "__main__":
