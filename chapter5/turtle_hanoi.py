bb8872541ce25579ec7e58d140d7d0440cc8aa33
riccardompesce <riccardompesce@gmail.com>
2021-10-16 18:49:58 +0200

    TODO: get initial positions right

--- chapter5/turtle_hanoi.py
+++ chapter5/turtle_hanoi.py
@@ -1,4 +1,5 @@
 import turtle 
+import random
 
 from pythonds3 import Stack
 
@@ -16,14 +17,30 @@ def move_disk(from_p, to_p):
 
 def main():
     turtle.setup(WIDTH, HEIGHT)
+    turtle.setworldcoordinates(0, 0, WIDTH, HEIGHT)
+    turtle.colormode(255)
 
     t1 = turtle.Turtle()
     t2 = turtle.Turtle()
     t3 = turtle.Turtle()
-
+    
     p1 = turtle.Turtle()
     p2 = turtle.Turtle()
-    p = turtle.Turtle()
+    p3 = turtle.Turtle()
+
+    for i, p in enumerate([p1, p2, p3]):
+        p.shape("square")
+        p.turtlesize(HEIGHT // 30, 1, None)
+        p.penup()
+        p.setposition((WIDTH // 3) * i + WIDTH // 6, 10)
+
+    for i, t in enumerate([t1, t2, t3]):
+        color = (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        t.shape("square")
+        t.color(*color)
+        t.turtlesize(1, i * (WIDTH // 300) + (WIDTH // 1000))
+        t.penup()
+        t.setpos(WIDTH // 6, 10 - i)
 
     turtle.exitonclick()
 
